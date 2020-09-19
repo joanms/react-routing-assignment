dac5471fae64a9b074532586686be4b60ca6aa83
Joan Sweeney <joanms@eircom.net>
2020-09-19 16:15:40 +0100

    Make the course titles clickable

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
+import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 
 class App extends Component {
@@ -28,8 +29,10 @@ class App extends Component {
             </li>
          </ul>
         </nav>
-        <Route path="/courses" component={Courses} />
+        {/* Can also use Switch for the following, but the first path should be last in that case */}
+        <Route path="/courses" exact component={Courses} />
         <Route path="/users" component={Users} />
+        <Route path="/courses/course" component={Course} />
       </div>
     );
   }
