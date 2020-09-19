b5c0f2082bab4c6e8ff97345aad5111e596b8b72
Joan Sweeney <joanms@eircom.net>
2020-09-19 17:15:23 +0100

    Add 404 error page for unknown routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Link, Switch } from 'react-router-dom';
 import Courses from './containers/Courses/Courses';
 import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
+import NotFound from './components/NotFound/NotFound';
 
 class App extends Component {
   render () {
@@ -33,6 +34,7 @@ class App extends Component {
           <Route path="/users" component={Users} />
           {/* <Route path="/courses/:courseId" component={Course} /> */}
           <Route path="/courses" component={Courses} />
+          <Route component={NotFound} />
         </Switch>
       </div>
     );
